548590379d91d5efa0f65ff7c0310b7e6689f65b
LHF009 <rodriguezpatricio567@gmail.com>
2024-08-10 23:46:51 -0300

    added cli

---
+++ src/domain/cli.rs
@@ -0,0 +1,55 @@
+use crate::domain::Blockchain;
+pub struct CLI {
+    pub bc: Blockchain,
+}
+
+impl CLI{
+    pub fn new(bc: Blockchain) -> Self{
+        CLI{bc: bc}
+    }
+
+    pub fn run(){
+        cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("data", "", "Block data")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+
+    }
+    fn validateArgs() {
+        if len(os.Args) < 2 {
+            cli.printUsage()
+            os.Exit(1)
+        }
+    }
+}
+
--- target/debug/deps/block_rust-41fcdc1e23f652ee.d
+++ target/debug/deps/block_rust-41fcdc1e23f652ee.d
@@ -1,9 +1,10 @@
-/Users/rodriguezpatricio/Desktop/Deving/Personal/block-rust/target/debug/deps/libblock_rust-41fcdc1e23f652ee.rmeta: src/main.rs src/domain/mod.rs src/domain/block.rs src/domain/blockchain.rs src/domain/proof_of_work.rs
+/Users/rodriguezpatricio/Desktop/Deving/Personal/block-rust/target/debug/deps/libblock_rust-41fcdc1e23f652ee.rmeta: src/main.rs src/domain/mod.rs src/domain/block.rs src/domain/blockchain.rs src/domain/cli.rs src/domain/proof_of_work.rs
 
-/Users/rodriguezpatricio/Desktop/Deving/Personal/block-rust/target/debug/deps/block_rust-41fcdc1e23f652ee.d: src/main.rs src/domain/mod.rs src/domain/block.rs src/domain/blockchain.rs src/domain/proof_of_work.rs
+/Users/rodriguezpatricio/Desktop/Deving/Personal/block-rust/target/debug/deps/block_rust-41fcdc1e23f652ee.d: src/main.rs src/domain/mod.rs src/domain/block.rs src/domain/blockchain.rs src/domain/cli.rs src/domain/proof_of_work.rs
 
 src/main.rs:
 src/domain/mod.rs:
 src/domain/block.rs:
 src/domain/blockchain.rs:
+src/domain/cli.rs:
 src/domain/proof_of_work.rs:

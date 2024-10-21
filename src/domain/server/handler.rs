88f42e4f882c1a92fd5fc5938b7122abd72b30d5
LHF009 <rodriguezpatricio567@gmail.com>
2024-10-20 21:46:32 -0300

    added server primitives

--- src/domain/server/handler.rs
+++ src/domain/server/handler.rs
@@ -1,4 +1,8 @@
+use std::io::Read;
 use std::net::TcpListener;
+use std::net::TcpStream;
+
+use crate::domain::Blockchain;
 
 pub fn start_server(node_id: String, miner_address: String) -> std::io::Result<()>{
     let mining_address = miner_address;
@@ -9,4 +13,27 @@ pub fn start_server(node_id: String, miner_address: String) -> std::io::Result<(
         //handle_client(stream?);
     }
     Ok(())
+}
+
+pub fn handle_connection(mut conn: TcpStream, bc: Blockchain){
+    let buffer = &mut Vec::new();
+    let request = conn.read_to_end( buffer);
+    match request{
+        Ok(res) =>{
+            match res{
+                1 => handle_address(),
+                2 => handle_get_blocks(request.unwrap(), bc),
+                _ => println!("Unknown command!")
+            }
+        }
+        Err(_)=> eprintln!("")
+    }
+}
+
+pub fn handle_address(){
+    println!("handling address...")
+}
+
+pub fn handle_get_blocks(request: usize, bc: Blockchain){
+    println!("handling address...")
 }

4ef929ce42deed4c0789dfb1e86b7378707ddca1
LHF009 <rodriguezpatricio567@gmail.com>
2025-01-17 21:36:20 -0300

    changed to tokio for better async handling

--- src/domain/server/handler.rs
+++ src/domain/server/handler.rs
@@ -2,9 +2,8 @@ use crate::domain::validate_address;
 use crate::domain::Blockchain;
 use crate::domain::Transaction;
 use std::collections::HashMap;
-use std::io::Read;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 
 pub struct Server {
     pub node_address: String,
@@ -24,19 +23,13 @@ impl Server {
             mem_pool: HashMap::<String, Transaction>::new(),
         };
     }
-    pub fn start_server(&mut self, _node_id: String, miner_address: String) -> std::io::Result<()> {
+    pub async fn start_server(
+        &mut self,
+        _node_id: String,
+        miner_address: String,
+    ) -> std::io::Result<()> {
         let mining_address = miner_address;
-        let result = TcpListener::bind("127.0.0.1:8000");
-        match &result {
-            Ok(_) => {
-                println!("Listener created successfully")
-            }
-            Err(e) => {
-                eprintln!("Something went wrong {:?}", e);
-                return Ok(());
-            }
-        }
-        let listener = result.unwrap();
+
         let mut bc: Blockchain;
         match Blockchain::create_blockchain(mining_address) {
             Ok(blockchain) => {
@@ -46,22 +39,19 @@ impl Server {
                 bc = Blockchain::new().unwrap();
             }
         }
-        println!("Blockchain created");
-        println!("Listening...");
-        for stream in listener.incoming() {
-            match stream {
-                Ok(conn) => self.handle_connection(conn, &mut bc),
-                Err(e) => {
-                    eprintln!("Sth went wrong {:?}", e);
-                }
-            }
+        let listener = TcpListener::bind("127.0.0.1:8000").await?;
+        loop {
+            let (socket, _) = listener.accept().await?;
+            self.handle_connection(socket, &mut bc).await;
         }
         Ok(())
     }
 
-    pub fn handle_connection(&mut self, mut conn: TcpStream, bc: &mut Blockchain) {
+    pub async fn handle_connection(&mut self, mut conn: TcpStream, bc: &mut Blockchain) {
         let mut command_buf = [0u8; 1];
-        match conn.read_exact(&mut command_buf) {
+        conn.readable().await;
+
+        match conn.try_read(&mut command_buf) {
             Ok(_) => {
                 let command = command_buf[0];
                 println!("Received command: {}", command);
@@ -79,9 +69,11 @@ impl Server {
         }
     }
 
-    pub fn handle_address(&mut self, mut conn: TcpStream) {
+    pub async fn handle_address(&mut self, mut conn: TcpStream) {
         let mut buffer = Vec::new();
-        match conn.read_to_end(&mut buffer) {
+        conn.readable().await;
+
+        match conn.try_read(&mut buffer) {
             Ok(_) => {
                 if let Ok(address) = String::from_utf8(buffer) {
                     println!("handling address: {:?}...", address);
